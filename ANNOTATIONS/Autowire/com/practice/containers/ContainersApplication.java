dba46aaee2127efa371210943119f81c4264897f
Rupesh Keesaram <75531784+RupeshKeesaram@users.noreply.github.com>
2022-08-09 19:26:50 +0530

    Create ContainersApplication.java

---
+++ ANNOTATIONS/Autowire/com/practice/containers/ContainersApplication.java
@@ -0,0 +1,33 @@
+package com.practice.containers;
+
+import com.practice.containers.beans.Person;
+import com.practice.containers.beans.Vehicle;
+import com.practice.containers.config.ProjectConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.function.Supplier;
+
+@SpringBootApplication
+public class ContainersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContainersApplication.class, args);
+
+		AnnotationConfigApplicationContext context  = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+		String[] persons = context.getBeanNamesForType(Person.class);
+		System.out.println("Persons :"+persons);
+
+//		 getting person bean & calling playmusic
+		Person person = context.getBean(Person.class);
+		person.getVehicle().getVehicleService().playMusic();
+
+
+//		getting vehicle bean & calling moveCar
+		Vehicle vehicle = context.getBean(Vehicle.class);
+		vehicle.getVehicleService().moveCar();
+	}
+
+}
