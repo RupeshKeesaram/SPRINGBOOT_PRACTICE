9d358059fe55cda76c44e18ae4d988d43917629a
Rupesh Keesaram <75531784+RupeshKeesaram@users.noreply.github.com>
2022-08-14 02:07:55 +0530

    Create ContainersApplication.java

---
+++ LOMBOK/@Data/com/practice/containers/ContainersApplication.java
@@ -0,0 +1,27 @@
+package com.practice.containers;
+
+import com.practice.containers.beans.Person;
+import com.practice.containers.beans.Vehicle;
+import com.practice.containers.config.ProjectConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.function.Supplier;
+
+@SpringBootApplication
+public class ContainersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContainersApplication.class, args);
+
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+		// checking getters and setters that were added using lombok
+
+		Person person = (Person) context.getBean("personBean");
+		System.out.println(person.getPersonName());
+
+	}
+
+}
