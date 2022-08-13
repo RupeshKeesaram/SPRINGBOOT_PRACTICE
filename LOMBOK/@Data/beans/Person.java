fada1fbc19b5976cf0779902174628b998800b8d
Rupesh Keesaram <75531784+RupeshKeesaram@users.noreply.github.com>
2022-08-14 02:14:37 +0530

    Update Person.java

--- LOMBOK/@Data/beans/Person.java
+++ LOMBOK/@Data/beans/Person.java
@@ -11,12 +11,14 @@ import javax.annotation.PostConstruct;
 
 
 @Component("personBean")
-// @Setter
-// @Getter
-//@RequiredArgsConstructor
-//@ToString
-//@EqualsAndHashCode
+// @Setter       -- provides all setters
+// @Getter       -- provides all getters
+//@RequiredArgsConstructor  -- provides constructor with required arguments (  Lombok also provides AllArgsConstructor & NoArgsConstructor )
+//@ToString                 -- Overrides toString() method
+//@EqualsAndHashCode        -- Overrides Equals method
+
 // @Data annotation from lombok enables all above five annotations
+
 @Data
 public class Person {
     String personName;
@@ -28,3 +30,12 @@ public class Person {
         this.personName="Rupesh";
     }
 }
+
+
+// @AllArgsConstructor generates a constructor with 1 parameter for each field in your class.
+
+// @RequiredArgsConstructor generates a constructor with 1 parameter for each field that requires special handling. 
+// All non-initialized final fields get a parameter, as well as any fields that are marked as @NonNull that aren't initialized where they are declared.
+
+// In short, use @AllArgsConstructor  to generate a constructor for all of your class's fields and use @RequiredArgsConstructor to generate 
+// a constructor for all class's fields that are marked as final.
